2fde2209981ddab099c779c1e3fc573e8bc18a4d
liulingtong18 <815361402@qq.com>
2018-04-26 10:17:57 +0800

    第七次 commit

--- controller/routes/getdata.js
+++ controller/routes/getdata.js
@@ -5,23 +5,17 @@ const router = Router();
 const getData = require('./getData/getData')
 
 router.get('/get',(req,res,next)=>{
+    if(req.session.user){
+        score.create({user_id:req.session.user._id,username:req.session.user.username,type:15,score: 0.1,scoreName: '学习党史'}).then(data=>{
+            console.log('success')
+        })
+    }
     getData().then(data => {
-        if(req.session.user){
-            score.create({user_id:req.session.user._id,username:req.session.user.username,type:15,score: 0.1,scoreName: '学习党史'}).then(data=>{
-                console.log('success')
-            })
-            res.json({
-                data: data,
-                code: 200,
-                msg: "success"
-            })
-        }else{
-            res.json({
-                data: data,
-                code: 200,
-                msg: "success"
-            })
-        }
+        res.json({
+            data: data,
+            code: 200,
+            msg: "success"
+        })
     })
 })
 module.exports = router
