6d72ba97fe43066bd13ec6f52739a62c8120c97b
liulingtong18 <815361402@qq.com>
2018-04-26 10:35:32 +0800

    第8次 commit

--- controller/routes/getdata.js
+++ controller/routes/getdata.js
@@ -3,13 +3,9 @@ const router = Router();
 
 
 const getData = require('./getData/getData')
+let score = require('../../config/model/score');
 
 router.get('/get',(req,res,next)=>{
-    if(req.session.user){
-        score.create({user_id:req.session.user._id,username:req.session.user.username,type:15,score: 0.1,scoreName: '学习党史'}).then(data=>{
-            console.log('success')
-        })
-    }
     getData().then(data => {
         res.json({
             data: data,
@@ -17,5 +13,11 @@ router.get('/get',(req,res,next)=>{
             msg: "success"
         })
     })
+    if(req.session.user){
+        score.create({user_id:req.session.user._id,username:req.session.user.username,type:15,score: 0.1,scoreName: '学习党史'}).then(data=>{
+            console.log('success')
+        })
+        return
+    }
 })
 module.exports = router
