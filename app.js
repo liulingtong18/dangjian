d639bf43460834c41a77e99f5cc82d155168b0e2
liulingtong18 <815361402@qq.com>
2018-04-26 15:19:13 +0800

    第5次 commit

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('./config/config')
 
 
-var index = require('./routes/index');
+// var index = require('./routes/index');
+var client = require('./controller/client');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// app.use('/', index);
+app.use('/', client);
 app.use("/api", require("./controller/index"));
 
 // catch 404 and forward to error handler
